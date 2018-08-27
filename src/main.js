223c4f882cee3d9a86ce5ab176006779aacf291c
liuzhixin02 <liuzhixin02@baidu.com>
2018-08-27 17:24:45 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import NormailizeCss from 'normalize.css'
+import NormailizeCss from 'normalize.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
-
+import axios from 'axios'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(NormailizeCss)
 Vue.component('icon', Icon)
+
+Object.defineProperty(Vue.prototype, '$axios', { value: axios });
 
 /* eslint-disable no-new */
 new Vue({
